064c47c2d5ad9e26fd6e873a50ac12693512af26
Jerome <jerome-pl2@hotmail.ca>
2015-12-31 11:08:33 -0500

    Display how many messages are pending

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,11 +1,21 @@
-app.controller('HomeController', ['$scope', '$location', 'facebookService', function($scope, $location, facebookService) {
+app.controller('HomeController', ['$scope', '$location', 'facebookService', 'pullService', function($scope, $location, facebookService, pullService) {
 
     $scope.firstName = "";
+    $scope.numMessages = "no"; //Used to display the number of pending messages. "no" is the same as 0 messages pending
 
     facebookService.getFirstName().then(function(response) {
          $scope.firstName = response;
     }, function(response) {
-        console.log(response);
+        console.log("Error: " + response);
+    });
+
+    pullService.getNumMessages().then(function(response) {
+        $scope.numMessages = response.data;
+
+        if($scope.numMessages == 0) //Edit the displayed text if there are no pending messages
+            $scope.numMessages = "no";
+    }, function(response) {
+        console.log("Error: " + response);
     });
 
     $scope.sendMessage = function() {
--- js/services/pullService.js
+++ js/services/pullService.js
@@ -1,11 +1,20 @@
 app.factory('pullService', ['$http', 'facebookService', function ($http, facebookService) {
 
     return {
+        getNumMessages: function() {
+            return facebookService.getID().then(function(response) {
+                return $http({
+                    method: 'GET',
+                    url: 'server_side/getNumMessages.php?user_id=' + response
+                });
+            });
+        },
+
         getMessages: function() { //Pushes a new message in the database
             return facebookService.getID().then(function(response) {
                 return $http({
                     method: 'GET',
-                    url: 'server_side/getMessages.php?user_id=' + response.id
+                    url: 'server_side/getMessages.php?user_id=' + response
                 }); //Returns a promise with the response
             });
         },
